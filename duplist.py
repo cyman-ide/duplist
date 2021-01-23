bf95c78651602d1a4a0b99df9de1042feb3d0747
cyman <cyman@cosmiccrew.net>
2021-01-23 11:21:36 -0800

    fix missing some duplicates

--- duplist.py
+++ duplist.py
@@ -7,7 +7,7 @@ def get_files(folder):
     for (root, folder_names, filenames) in os.walk(folder):
         for filename in filenames:
             full_path = os.path.join(root, filename)
-            file_list.append((os.path.getsize(full_path), os.path.relpath(full_path, folder)))
+            file_list.append((os.path.getsize(full_path), full_path))
     return file_list
 
 def get_duplicates(folder):
@@ -16,11 +16,17 @@ def get_duplicates(folder):
     file_list.sort()
 
     duplicates = []
-    for i in range(len(file_list)-1):
-        if file_list[i][0] == file_list[i+1][0]:
-            duplicates.append(file_list[i])
-            duplicates.append(file_list[i+1])
-            i += 1
+    last_item = file_list[0]
+    dup_count = 0
+    for item in file_list[1:]:
+        if item[0] == last_item[0]:
+            if dup_count == 0:
+                duplicates.append(last_item)
+            duplicates.append(item)
+            dup_count += 1
+        else:
+            dup_count = 0
+        last_item = item
 
     return duplicates
 
@@ -35,15 +41,16 @@ def data_size_fmt(size):
         
 if __name__ == "__main__":
     if len(sys.argv) > 1 and os.path.isdir(sys.argv[1]):
+        folder = sys.argv[1]
         last_item_size = 0
-        duplicates = get_duplicates(sys.argv[1])
+        duplicates = get_duplicates(folder)
         if len(duplicates) == 0:
             print("no duplicates found")
         else:
             print("potential duplicates found:")
             for item in duplicates:
                 if item[0] != last_item_size: print()
-                print("(%s) %s" % (data_size_fmt(item[0]), item[1]))
+                print( "(%s) %s" % (data_size_fmt(item[0]), os.path.relpath(item[1], folder)) )
                 last_item_size = item[0]
     else:
         print("error: no directory provided")
